3aaf6d50582e29300cd7d09c9f9db970b5d45264
Matt Cano <cano.matt@gmail.com>
2013-04-08 16:55:07 -0700

    Search results not found

--- movies.rb
+++ movies.rb
@@ -21,16 +21,27 @@ class Movies < Sinatra::Base
   get "/search" do
     @query = params[:q]
     @button = params[:button]
+    @page_title += ": Search Results for #{@query}"
     file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
-    @results = JSON.load(file.read)["Search"]
-    erb :results
+    @results = JSON.load(file.read)["Search"] || []
+    if @results.size == 1
+      redirect "/movies"
+    else
+      erb :results
+    end
   end
 
   get "/movies" do
     @id = params[:id]
-    file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}")
+    file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
     @result = JSON.load(file.read)
+    set_actors_and_directors(@result)
     erb :detail
+  end
+
+  def set_actors_and_directors(result)
+    @actors = result["Actors"].split(", ")
+    @directors = result["Director"].split(", ")
   end
 
   run!
