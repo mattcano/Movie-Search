3b2f752b46674ff1505cfe430792edc7a1a5bdfa
Matt Cano <cano.matt@gmail.com>
2013-04-09 11:53:22 -0700

    Add gemfile and config.ru for deployment

--- movies.rb
+++ movies.rb
@@ -1,56 +1,57 @@
-require "sinatra/base"
+require "sinatra"
 require "sinatra/reloader" #if development?
 require "open-uri"
 require "json"
+require "date"
 
-class Movies < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
+configure :development do
+  register Sinatra::Reloader
+end
 
-  before do
-    @app_name = "Movie App"
-    @page_title = @app_name
-  end
+before do
+  @app_name = "Movie App"
+  @page_title = @app_name
+end
 
-  get "/" do
-    @page_title += ": Home"
-    erb :home
-  end
+get "/" do
+  @page_title += ": Home"
+  erb :home
+end
 
-  get "/search" do
-    @query = params[:q]
-    @button = params[:button]
-    if @button == "lucky"
-      file = open("http://www.omdbapi.com/?t=#{URI.escape(@query)}&tomatoes=true")
-      @result = JSON.load(file.read)  
-      @page_title += ": #{@result["Title"]}"
-      set_actors_and_directors(@result)
-      erb :detail
-    else
-      @page_title += ": Search Results for #{@query}"
-      file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
-      @results = JSON.load(file.read)["Search"] || []
-      if @results.size == 1
-        redirect "/movies"
-      else
-        erb :results
-      end
-    end
-  end
 
-  get "/movies" do
-    @id = params[:id]
-    file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
-    @result = JSON.load(file.read)
+#make search results as its own ERB file so it can be replicated in related results
+get "/search" do
+  @query = params[:q]
+  @button = params[:button]
+  if @button == "lucky"
+    file = open("http://www.omdbapi.com/?t=#{URI.escape(@query)}&tomatoes=true")
+    @result = JSON.load(file.read)  
+    @page_title += ": #{@result["Title"]}"
     set_actors_and_directors(@result)
     erb :detail
+  else
+    @page_title += ": Search Results for #{@query}"
+    file = open("http://www.omdbapi.com/?s=#{URI.escape(@query)}")
+    @results = JSON.load(file.read)["Search"] || []
+    if @results.size == 1
+      redirect "/movies"
+    else
+      erb :results
+    end
   end
+end
 
-  def set_actors_and_directors(result)
-    @actors = result["Actors"].split(", ")
-    @directors = result["Director"].split(", ")
-  end
+#when doing related results, use the code below to remove current movie from related list
+#@results.reject!{|movie| movie["imdbID"] == @result["imdbID"]}
+get "/movies" do
+  @id = params[:id]
+  file = open("http://www.omdbapi.com/?i=#{URI.escape(@id)}&tomatoes=true")
+  @result = JSON.load(file.read)
+  set_actors_and_directors(@result)
+  erb :detail
+end
 
-  run!
+def set_actors_and_directors(result)
+  @actors = result["Actors"].split(", ")
+  @directors = result["Director"].split(", ")
 end
